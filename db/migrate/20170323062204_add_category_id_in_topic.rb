c7e2d94aaa6e1111f6caa3e7c7a611a7f8990d2e
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-03-23 14:26:57 +0800

    add category has_many topics

---
+++ db/migrate/20170323062204_add_category_id_in_topic.rb
@@ -0,0 +1,5 @@
+class AddCategoryIdInTopic < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :topics, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322063928) do
+ActiveRecord::Schema.define(version: 20170323062204) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
