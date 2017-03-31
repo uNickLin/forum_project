a0025b066c88da8cb9b4a10cbc78a54de05648ed
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-03-31 12:31:06 +0800

    view topic's all comment users in show page

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,4 +7,13 @@ class Topic < ApplicationRecord
 	belongs_to :user
 
 	has_many :comments, dependent: :destroy
+
+  def group_by_comments_users(topic)
+    comments_users = []
+    topic.comments.each do |j|
+      comments_users << j.user.nickname
+    end
+
+    return comments_users.uniq
+  end
 end
